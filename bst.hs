74fcec15c8057f1edd5438861115be15fb1edb3a
Chase Ries <hello@chaseries.com>
2017-02-27 16:52:55 -0500

    Add delete function

--- bst.hs
+++ bst.hs
@@ -20,11 +20,23 @@ contains (Node l v r) x
   | v < x = contains r x
   | otherwise = True
 
-delete :: (Ord a) => Bst a -> a -> Bst a
-delete (Node l v r) x
-  | v > x = Node (delete l x) v r
-  | v < x = Node l v (delete r x)
-  | otherwise = Leaf
+del :: (Ord a) => Bst a -> a -> Bst a
+del Leaf _ = Leaf
+del (Node l v r) x
+  | v > x = Node (del l x) v r
+  | v < x = Node l v (del r x)
+  | otherwise = merge l r
+
+merge :: (Ord a) => Bst a -> Bst a -> Bst a
+merge Leaf Leaf = Leaf
+merge Leaf n = n
+merge n Leaf = n
+merge n1@(Node l1 v1 r1) n2@(Node l2 v2 r2)
+  | v1 > v2 = Node (merge n2 l1) v1 r1
+  | otherwise = Node (merge n1 l2) v2 r2
+
+getVal :: (Ord a) => Bst a -> a
+getVal (Node l v r) = v
 
 toList :: (Ord a) => Bst a -> [a]
 toList Leaf = []
@@ -33,13 +45,26 @@ toList (Node l v r) = toList l ++  [v] ++ toList r
 middleEl :: (Ord a) => [a] -> a
 middleEl l = head rh
   where
-    (_, rh) = splitAt (length l `div` 2) l
+    (_, rh) = listToHalves l
+
+listToHalves :: (Ord a) => [a] -> ([a], [a])
+listToHalves l = splitAt (length l `div` 2) l
+
+listToThree :: (Ord a) => [a] -> ([a], a, [a])
+listToThree l = (lh, middleEl l, drop 1 rh)
+  where
+    (lh, rh) = listToHalves l
+
+balanceHelp :: (Ord a) => [a] -> Bst a
+balanceHelp [] = Leaf
+balanceHelp l = Node (balanceHelp lh) m (balanceHelp rh)
+  where
+    (lh, m, rh) = listToThree l
 
 balance :: (Ord a) => Bst a -> Bst a
 balance Leaf = Leaf
-balance bst@(Node l v r) =
-
-
+balance bst = balanceHelp l
+  where l = toList bst
 
 ------------
 -- Sample stuff 
@@ -50,3 +75,6 @@ three = insert two 20
 four = insert three 2
 five = insert four 11
 six = insert five 8
+sampleTree = insert six 13
+
+unbalanced = (Node Leaf 1 (Node Leaf 2 (Node Leaf 3 (Node Leaf 4 (Node Leaf 5 (Node Leaf 7 Leaf))))))
